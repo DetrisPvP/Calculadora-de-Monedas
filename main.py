05bd37b7dd835d089153cd9e4f851cc62d8ebf01
DetrisPvP <57355045+DetrisPvP@users.noreply.github.com>
2025-04-24 20:39:15 -0400

    Moneda Cambiada

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 def exchange_money(budget, exchange_rate):
-    """
-    Convierte el presupuesto ingresado a la moneda extranjera usando la tasa de cambio.
 
-    Parámetros:
-    - budget: cantidad de dinero en moneda original (float)
-    - exchange_rate: tasa de cambio (float)
-
-    Retorna:
-    - El valor convertido en la moneda extranjera (float)
-    """
     return budget * exchange_rate
 
 # Prueba 1 de Japón
 print("300 USD a yenes:", exchange_money(300, 133.33))
 
 # Prueba 2 de México
-print("500 USD a pesos mexicanos:", exchange_money(500, 16.95))
+print("500 USD a pesos mexicanos:", exchange_money(500, 19.15))
 
 # Prueba 3 de Alemania
 print("200 USD a euros:", exchange_money(200, 0.935))
